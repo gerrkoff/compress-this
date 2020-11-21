f29bce67d5ff8f46074a1762dd431acbbe90a36b
gerrkoff <antonkofa@gmail.com>
2020-11-21 22:21:02 +0300

    adjust script

--- test-speed.sh
+++ test-speed.sh
@@ -15,10 +15,10 @@ do
   SECONDS=0
   echo "=== Iteration $X"
   dotnet "$APP" compress "$ORIGINAL" "$COMPRESSED" "$1" "$2"
-  DURATION=$(( $DURATION + $SECONDS ))
-  X=$(( $X + 1 ))
+  DURATION=$(( DURATION + SECONDS ))
+  X=$(( X + 1 ))
 done
 
-X=$(( $X - 1 ))
+X=$(( X - 1 ))
 
 echo "TIME $((DURATION / 60))m $((DURATION % 60))s | AVG $((DURATION / X)).$((DURATION % X))/$((X))s "
