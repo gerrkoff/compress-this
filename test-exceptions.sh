f37f4ca16201a3553c2d6096573a11b193adf169
gerrkoff <antonkofa@gmail.com>
2020-11-22 13:57:51 +0300

    adjust test exceptions

--- test-exceptions.sh
+++ test-exceptions.sh
@@ -6,18 +6,18 @@ APP="test/build/CompressThis.dll"
 FILE_10="test/big10.txt"
 FILE_20000="test/big20000.txt"
 
-#echo '=== case 1'
-#dotnet "$APP" compress "$FILE_20000" "$FILE_20000.gz" --block-size 1
-#
-#echo '=== case 2'
-#dotnet "$APP"
-#
-#echo '=== case 3'
-#dotnet "$APP" decompress "$FILE_10" "$FILE_10.d" -s
-#
-#echo '=== case 4'
-#dotnet "$APP" decompress "$FILE_10" "$FILE_10.d"
+echo '=== case 0'
+dotnet "$APP"
 
-echo '=== case 5'
+echo '=== case 1'
+dotnet "$APP" compress "$FILE_20000" "$FILE_20000.gz" --block-size 1
+
+echo '=== case 2'
+dotnet "$APP" decompress "$FILE_10" "$FILE_10.d" -s
+
+echo '=== case 3'
+dotnet "$APP" decompress "$FILE_10" "$FILE_10.d"
+
+echo '=== case 4'
 dotnet "$APP" compress "$FILE_10" "$FILE_10.gz" -s
 dotnet "$APP" decompress "$FILE_10.gz" "$FILE_10.d"
